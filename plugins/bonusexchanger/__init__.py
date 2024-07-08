c54463630c1c23a255c3a246401323cfd279c6c2
jie <1028361767@qq.com>
2024-07-08 18:38:53 +0800

    茉莉兑换v0.1
    1.插件初始化
    2.表单初始化

---
+++ plugins/bonusexchanger/__init__.py
@@ -0,0 +1,213 @@
+import threading
+from typing import List, Tuple, Dict, Any
+
+from app.db.transferhistory_oper import TransferHistoryOper
+from app.log import logger
+from app.plugins import _PluginBase
+
+state_lock = threading.Lock()
+
+class BonusExchanger(_PluginBase):
+    # 插件名称
+    plugin_name = "茉莉兑换器"
+    # 插件描述
+    plugin_desc = "兑换上传、下载..."
+    # 插件图标
+    plugin_icon = "BonusExchanger.png"
+    # 插件版本
+    plugin_version = "0.1"
+    # 插件作者
+    plugin_author = "anxinJ"
+    # 作者主页
+    author_url = "https://github.com/anxinJ"
+    # 插件配置项ID前缀
+    plugin_config_prefix = "bonusexchanger_"
+    # 加载顺序
+    plugin_order = 0
+    # 可使用的用户级别
+    auth_level = 1
+
+    # preivate property
+    exchange_sites = ""
+    exchange_type = ""
+    request_frequence = ""
+    _enabled = False
+    _notify = False
+    _onlyonce = False
+    _observer = []
+
+    def init_plugin(self, config: dict = None):
+        logger.info(f"Hello, RemoveLink! config {config}")
+        self._transferhistory = TransferHistoryOper()
+        if config:
+            self._enabled = config.get("enabled")
+            self._notify = config.get("notify")
+            self._onlyonce = config.get("onlyonce")
+            self.exchange_sites = config.get("exchange_sites")
+            self.exchange_type = config.get("exchange_type")
+            self.request_frequence = config.get("request_frequence")
+
+        if self._enabled:
+            # 读取兑换站点
+            exchange_sites = self.exchange_sites.split("\n")
+            logger.info(f"兑换站点：{exchange_sites}")
+            logger.info(f"兑换类型：{self.exchange_type}")
+            logger.info(f"请求频率：{self.request_frequence}")
+            if not exchange_sites:
+                return
+
+
+    def __update_config(self):
+        """
+        更新配置
+        """
+        self.update_config(
+            {
+                "enabled": self._enabled,
+                "notify": self._notify,
+                "onlyonce": self._onlyonce,
+                "exchange_sites": self.exchange_sites,
+                "exchange_type": self.exchange_type,
+                "request_frequence": self.request_frequence
+            }
+        )
+
+    def get_state(self) -> bool:
+        return self._enabled
+
+    @staticmethod
+    def get_command() -> List[Dict[str, Any]]:
+        pass
+
+    def get_api(self) -> List[Dict[str, Any]]:
+        pass
+
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+        site_options = [{"title": site.get("name"), "value": site.get("id")}
+                        for site in self.siteshelper.get_indexers()]
+        return [
+            {
+                "component": "VForm",
+                "content": [
+                    {
+                        "component": "VRow",
+                        "content": [
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
+                                    {
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "enabled",
+                                            "label": "启用插件",
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
+                                    {
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "notify",
+                                            "label": "发送通知",
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'onlyonce',
+                                            'label': '立即运行一次',
+                                        }
+                                    }
+                                ]
+                            }
+                        ],
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSelect',
+                                        'props': {
+                                            'multiple': True,
+                                            'chips': True,
+                                            'clearable': True,
+                                            'model': 'exchange_sites',
+                                            'label': '兑换站点',
+                                            'items': site_options
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "component": "VRow",
+                        "content": [
+                            {
+                                "component": "VCol",
+                                "props": {
+                                    "cols": 12,
+                                },
+                                "content": [
+                                    {
+                                        "component": "VAlert",
+                                        "props": {
+                                            "type": "info",
+                                            "variant": "tonal",
+                                            "text": "茉莉兑换为测试功能，请谨慎开启。",
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                "component": "VCol",
+                                "props": {
+                                    "cols": 12,
+                                },
+                                "content": [
+                                    {
+                                        "component": "VAlert",
+                                        "props": {
+                                            "type": "info",
+                                            "variant": "tonal",
+                                            "text": "不同站点之间兑换类型可能不一致，导致兑换错误。等待适配站点",
+                                        },
+                                    }
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            }
+        ], {
+            "enabled": False,
+            "onlyonce": False,
+            "notify": False,
+            "exchange_sites": "",
+            "exchange_type": "upload",
+            "request_frequence": 11
+        }
+
+    def get_page(self) -> List[dict]:
+        pass
+
